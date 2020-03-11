5d399f814ad042d10f1493cfd9493c16228a89cf
Swami Developer Man <swamideveloperman@gmail.com>
2020-03-11 20:06:14 +0530

    Initial Commit

---
+++ adTemplates/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  adTemplates
+//
+//  Created by swami developer man on 11/03/20.
+//  Copyright © 2020 swami developer man. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
