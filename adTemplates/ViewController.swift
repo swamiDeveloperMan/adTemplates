7815dd76109114a1fea2d13f44e13adc5a674b32
Swami Developer Man <swamideveloperman@gmail.com>
2020-03-12 21:41:04 +0530

    initial commit

--- adTemplates/ViewController.swift
+++ adTemplates/ViewController.swift
@@ -7,14 +7,80 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bannerView: GADBannerView!
+    @IBOutlet weak var scoreLabel: UIButton!
+    
+    var interstitial: GADInterstitial!
+    var rewardedAd: GADRewardedAd!
+    
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initialSetup()
+        bannerView.load(GADRequest())
+    }
+    
+    fileprivate func initialSetup(){
+        scoreLabel.setTitle("\(score)", for: .normal)
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.rootViewController = self
+        interstitial = createAndLoadInterstitial()
+        rewardedAd = createAndLoadRewardedAd()
     }
 
-
+    @IBAction func viewInterstitial(_ sender: UIButton) {
+        if interstitial.isReady {
+            interstitial.present(fromRootViewController: self)
+        } else {
+            print("interstitial ad not ready")
+        }
+    }
+    @IBAction func viewVideo(_ sender: UIButton) {
+        if rewardedAd.isReady {
+            rewardedAd.present(fromRootViewController: self, delegate: self)
+        } else {
+            print("video ad not ready")
+        }
+    }
 }
 
+extension ViewController: GADInterstitialDelegate{
+    func interstitialDidDismissScreen(_ ad: GADInterstitial) {
+        interstitial = createAndLoadInterstitial()
+    }
+    func createAndLoadInterstitial() -> GADInterstitial {
+      var interstitial = GADInterstitial(adUnitID: "ca-app-pub-3940256099942544/4411468910")
+      interstitial.delegate = self
+      interstitial.load(GADRequest())
+      return interstitial
+    }
+}
+
+
+extension ViewController: GADRewardedAdDelegate{
+    func rewardedAd(_ rewardedAd: GADRewardedAd, userDidEarn reward: GADAdReward) {
+        score += Int(truncating: reward.amount)
+        scoreLabel.setTitle("\(score)", for: .normal)
+    }
+    
+    func rewardedAdDidDismiss(_ rewardedAd: GADRewardedAd) {
+        self.rewardedAd = createAndLoadRewardedAd()
+    }
+    
+    func createAndLoadRewardedAd() -> GADRewardedAd{
+      rewardedAd = GADRewardedAd(adUnitID: "ca-app-pub-3940256099942544/1712485313")
+      rewardedAd?.load(GADRequest()) { error in
+        if let error = error {
+          print("Loading failed: \(error)")
+        } else {
+          print("Loading Succeeded")
+        }
+      }
+      return rewardedAd
+    }
+}
